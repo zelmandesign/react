94a9ee61d5b320ca5766e03e41ec07c732dbd1f1
Andrzej <andrzej@zelmandesign.com>
2019-08-02 14:30:55 +0200

    moving further

---
+++ src/MyInfo.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function MyInfo() {
+	return (
+		<div className="App">
+			<h1>Big z</h1>
+			lorem ipsum
+			<ul style={{ listStyle: "none", padding: "0", margin: "0" }}>
+				<li>1</li>
+			</ul>
+		</div>
+	);
+}
+
+export default MyInfo;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import MyInfo from "./MyInfo";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<MyInfo />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
